5e9a670fc4fdb6b8807207e56a92ea24e412def9
foxokbrowser <85384447+foxokbrowser@users.noreply.github.com>
2021-08-07 15:22:04 +0800

    Update CompatibleDesktopMode.user.js

--- CompatibleDesktopMode.user.js
+++ CompatibleDesktopMode.user.js
@@ -4,18 +4,21 @@
 // @description  Compatible Desktop Mode
 // @author       Foxok
 // @include      www.aliyundrive.com
+// updateURL     https://raw.githubusercontent.com/foxokbrowser/scripts/main/CompatibleDesktopMode.user.js
+// downloadURL   https://raw.githubusercontent.com/foxokbrowser/scripts/main/CompatibleDesktopMode.user.js
+// supportURL    https://github.com/foxokbrowser/scripts/blob/main/CompatibleDesktopMode.user.js
 // @run-at       document-start
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var oMeta = document.createElement('meta');
+    let meta = document.createElement('meta');
     let DEFAULT_WIDTH = 640,
         ua = navigator.userAgent.toLowerCase(),
         deviceWidth = window.screen.width,
         devicePixelRatio = window.devicePixelRatio || 1,
         targetDensitydpi = DEFAULT_WIDTH / deviceWidth * devicePixelRatio * 160;
-    oMeta.content = 'target-densitydpi=' + targetDensitydpi;
-    oMeta.name = 'viewport';
-    document.getElementsByTagName('head')[0].appendChild(oMeta);
+    meta.content = 'target-densitydpi=' + targetDensitydpi;
+    meta.name = 'viewport';
+    document.getElementsByTagName('head')[0].appendChild(meta);
 })();
